4a591ddc1f4f5deb5f892cb858803c15d616f11e
Vladimir Elsukov <GroupDirect@yandex.ru>
2024-06-06 16:52:30 +0300

    Реализован class MetricToImperialConverter

---
+++ Test.py
@@ -0,0 +1,8 @@
+euros_count = 100
+
+# BEGIN (write your solution here)
+convert_euro_dollar = euros_count * 1.25
+print(convert_euro_dollar)
+convert_dollar_rub = 60
+print(convert_euro_dollar * convert_dollar_rub)
+# END
