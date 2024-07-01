ef736e6747f2180f7b1676fe4953c9de3caa2712
Vladimir Elsukov <GroupDirect@yandex.ru>
2024-07-01 10:23:41 +0300

    Реализован принцип инверсии зависимостей

--- InversionZavisimosti.py
+++ InversionZavisimosti.py
@@ -16,3 +16,45 @@ def getPermissions(self):
 self.auth.do_authentication()
 
 '''
+
+from abc import ABC, abstractmethod
+
+class Authentication(ABC):
+    @abstractmethod
+    def do_authentication(self):
+        ...
+
+class AnonymousAuthentication(Authentication):
+    def do_authentication(self):
+        print("Анонимная аутентификация: дополнительная аутентификация не требуется")
+
+class GithubAuthentication(Authentication):
+    def do_authentication(self):
+        print("Аутентификация на GitHub: аутентификация с помощью GitHub OAuth")
+
+class FacebookAuthentication(Authentication):
+    def do_authentication(self):
+        print("Аутентификация на Facebook: аутентификация с помощью Facebook OAuth")
+
+class Permissions:
+    def __init__(self, auth: Authentication):
+        self.auth = auth
+
+    def get_permissions(self):
+        self.auth.do_authentication()
+        print("Получение разрешений пользователя на основе аутентификации")
+
+# Создаем экземпляры классов аутентификации
+anonymous_auth = AnonymousAuthentication()
+github_auth = GithubAuthentication()
+facebook_auth = FacebookAuthentication()
+
+# Создаем экземпляры Permissions, используя разные типы аутентификации
+anonymous_permissions = Permissions(anonymous_auth)
+github_permissions = Permissions(github_auth)
+facebook_permissions = Permissions(facebook_auth)
+
+# Вызываем метод get_permissions() для каждого экземпляра Permissions
+anonymous_permissions.get_permissions()
+github_permissions.get_permissions()
+facebook_permissions.get_permissions()
